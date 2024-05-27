994ce7eb3607e907eaca1930e060e2da1269c3fc
LUCIANA MICAELA DOMINGUEZ SZTERN <48039458@ort>
2024-05-27 08:54:33 -0300

    Creación clase Paquete

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_Minuchin_Dominguez.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
