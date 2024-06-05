ece57c07800462950b42776bbf968919e3311ca6
LUCIANA MICAELA DOMINGUEZ SZTERN <48039458@ort>
2024-06-05 09:04:31 -0300

    View Index + view Select Paquete

--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -1,9 +1,9 @@
 public class ORTWorld
 {
-    public static List<string> ListaDestinos {get;private set;} = new List<string> () {"Nueva York","Roma","Londres","Atenas","Paris","Madrid","Brujas","Munich","Tel Aviv","Lisboa"};
-    public static List<string> ListaHoteles  {get;private set;} = new List<string> () {"Hilton","Four Seasons","Sheraton","InterContinental","Marriott","BTH","IHG","Accor","Choice","Ritz"};
-    public static List<string> ListaAereos  {get;private set;} = new List<string> (){"Iberia","Air France","British Airways","Lufthansa","American Airlines","RyanAir","Alitalia","Turkish Airlines","Emirates","Qatar Airways"};
-    public static List<string> ListaExcursiones {get;private set;} = new List<string> (){"Empire State","Coliseo","Big Ben", "Pantenon","Torre Eiffel","Palacio Real","Paseo en Barco","Tour Museos","Mar Muerto","Tour en Tranvia" };
+    public static List<string> ListaDestinos {get;private set;} = new List<string> () {"Nueva York.jpg","Roma.jpg","Londres.jpg","Atenas.jpg","Paris.jpeg","Madrid.jpg","Brujas.jpg","Munich.jpg","Tel Aviv.jpg","Lisboa.jpg"};
+    public static List<string> ListaHoteles  {get;private set;} = new List<string> () {"Hilton.jpg","Four Seasons.jpg","Sheraton.jpg","InterContinental.jfif","Marriott.jpg","BTH.jpg","IHG.jpg","Accor.jpg","Choice.jpg","Ritz.jpg"};
+    public static List<string> ListaAereos  {get;private set;} = new List<string> (){"Iberia.jpg","Air France.jpg","British Airways.png","Lufthansa.jpg","American Airlines.jpg","RyanAir.jpg","Alitalia.jpg","Turkish Airlines.jpg","Emirates.png","Qatar Airways.png"};
+    public static List<string> ListaExcursiones {get;private set;} = new List<string> (){"Empire State.jpg","Coliseo.jpg","Big Ben.jpg", "Pantenon.jpg","Torre Eiffel.png","Palacio Real.jpg","Paseo en Barco.jpg","Tour Museos.jpg","Mar Muerto.jpg","Tour en Tranvia.jpg" };
     public static Dictionary<string,Paquete> Paquetes {get; private set;}
 
     public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete)
